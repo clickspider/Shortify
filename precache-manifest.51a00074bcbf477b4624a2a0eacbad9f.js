d7b03b59bfb1793ee455ed2c69e32bbd94e94677
Clickspider <daniel@hype.co.il>
2019-11-29 21:10:27 +0200

    fixed btn styling + small bug

---
+++ precache-manifest.51a00074bcbf477b4624a2a0eacbad9f.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f88afec60de3341ce73b",
+    "url": "/css/app.2e5f2d60.css"
+  },
+  {
+    "revision": "dc8b886b4cf2a2426a6664ad17ebf7f9",
+    "url": "/img/bg-boost-desktop.dc8b886b.svg"
+  },
+  {
+    "revision": "47ef0c281d9e90c932dfd63faa38b94f",
+    "url": "/img/bg-boost-mobile.47ef0c28.svg"
+  },
+  {
+    "revision": "a82de60e4b42d5b6b6abf66a03a2dca1",
+    "url": "/img/bg-shorten-desktop.a82de60e.svg"
+  },
+  {
+    "revision": "4a7a7f615fb37565a0a706a7379dfb7e",
+    "url": "/img/bg-shorten-mobile.4a7a7f61.svg"
+  },
+  {
+    "revision": "1922b68a4d1dce29b2c40904cdbc1bba",
+    "url": "/img/icon-brand-recognition.1922b68a.svg"
+  },
+  {
+    "revision": "e3d9caa9df26426d598106f7f7289625",
+    "url": "/img/icon-detailed-records.e3d9caa9.svg"
+  },
+  {
+    "revision": "cc099cea0da2f5ae13aef182c107a854",
+    "url": "/img/icon-fully-customizable.cc099cea.svg"
+  },
+  {
+    "revision": "ed3d1277fa2859a11853b4945840b15a",
+    "url": "/img/illustration-working-mobile.ed3d1277.svg"
+  },
+  {
+    "revision": "5029d0493c803aac6c96e13be419ecb7",
+    "url": "/img/illustration-working.5029d049.svg"
+  },
+  {
+    "revision": "06e085a1d8218590d2c72d0cc9dfb661",
+    "url": "/img/logo-white.06e085a1.svg"
+  },
+  {
+    "revision": "6387840f9b6e8720165db8c73185bc51",
+    "url": "/img/logo.6387840f.svg"
+  },
+  {
+    "revision": "f3ebdbdadf043836d7c3b26096a2141c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f88afec60de3341ce73b",
+    "url": "/js/app.4d7ab9f5.js"
+  },
+  {
+    "revision": "cbeb203b8820065da3eb",
+    "url": "/js/chunk-vendors.426ac548.js"
+  },
+  {
+    "revision": "a65ebda02c656a48b971a1585094a66b",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
